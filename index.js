cf10a8e346e9fc8ad20bd32e000d313b6134c58a
Prasad Phule <prasadphule@live.com>
2023-06-30 21:54:13 +0530

    added indexjs file

---
+++ index.js
@@ -0,0 +1,38 @@
+
+
+//user variable with name age and 3 cars(tata, honda, tesla), address -city, state
+
+//index- array
+let users = [
+    {
+        name: "prasad",
+        age: 5,
+        cars: ['tata', 'honda', 'tesla'],
+        address: {
+            city: "pune",
+            state: 'maharastra',
+
+        },
+        isActive: false
+    },
+    {
+        name: "pranav",
+        age: 6,
+        cars: ['tata', 'honda', 'tesla'],
+        address: {
+            city: "mumbai",
+            state: 'maharastra',
+
+        },
+        isActive: false
+    }
+]
+
+//boundray condition
+
+//hint: dot operator
+console.log(users[0].address.city) //printing statement
+
+//execution environment //JavaScript runtime environment
+
+city: <div>users[0].address.city</div>
